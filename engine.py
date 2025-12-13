f06af696b93745aa970b5f5ab4d58d449b41387a
Kold <myself@koldinium.com>
2025-12-13 22:16:56 +0500

    More complex Piece Square Table Scheme

--- engine.py
+++ engine.py
@@ -52,6 +52,7 @@ class Engine:
             self.board.pop() # unmake move
             return val # return
 
+
     def evaluate(self):
         outcome = self.board.outcome()
         if outcome != None:
@@ -64,19 +65,91 @@ class Engine:
         else:
                 score = 0 
                 piece_values = {
-                    chess.PAWN: 1,
-                    chess.KNIGHT: 3,
-                    chess.BISHOP: 3,
-                    chess.ROOK: 5,
-                    chess.QUEEN: 9,
+                    chess.PAWN: 100,
+                    chess.KNIGHT: 300,
+                    chess.BISHOP: 300,
+                    chess.ROOK: 500,
+                    chess.QUEEN: 900,
+                    chess.KING: 1000,
                 }
-                for piece in self.board.piece_map().values():
-                    if piece.piece_type == chess.KING:
-                        continue
-                    value = piece_values[piece.piece_type]
-                    if piece.color: # is it white
-                        score += value
-                    else: # is it black
-                        score -= value
-                        
+                PIECE_SQUARE_TABLES = {
+                    chess.PAWN: [
+                        0,  0,  0,  0,  0,  0,  0,  0,
+                        50, 50, 50, 50, 50, 50, 50, 50,
+                        10, 10, 20, 30, 30, 20, 10, 10,
+                        5,  5, 10, 25, 25, 10,  5,  5,
+                        0,  0,  0, 20, 20,  0,  0,  0,
+                        5, -5,-10,  0,  0,-10, -5,  5,
+                        5, 10, 10,-20,-20, 10, 10,  5,
+                        0,  0,  0,  0,  0,  0,  0,  0
+                    ],
+                    chess.KNIGHT: [
+                        -50,-40,-30,-30,-30,-30,-40,-50,
+                        -40,-20,  0,  0,  0,  0,-20,-40,
+                        -30,  0, 10, 15, 15, 10,  0,-30,
+                        -30,  5, 15, 20, 20, 15,  5,-30,
+                        -30,  0, 15, 20, 20, 15,  0,-30,
+                        -30,  5, 10, 15, 15, 10,  5,-30,
+                        -40,-20,  0,  5,  5,  0,-20,-40,
+                        -50,-40,-30,-30,-30,-30,-40,-50
+                    ],
+                    chess.BISHOP: [
+                        -20,-10,-10,-10,-10,-10,-10,-20,
+                        -10,  0,  0,  0,  0,  0,  0,-10,
+                        -10,  0,  5, 10, 10,  5,  0,-10,
+                        -10,  5,  5, 10, 10,  5,  5,-10,
+                        -10,  0, 10, 10, 10, 10,  0,-10,
+                        -10, 10, 10, 10, 10, 10, 10,-10,
+                        -10,  5,  0,  0,  0,  0,  5,-10,
+                        -20,-10,-10,-10,-10,-10,-10,-20
+                    ],
+                    chess.ROOK: [
+                        0,  0,  0,  0,  0,  0,  0,  0,
+                        5, 10, 10, 10, 10, 10, 10,  5,
+                        -5,  0,  0,  0,  0,  0,  0, -5,
+                        -5,  0,  0,  0,  0,  0,  0, -5,
+                        -5,  0,  0,  0,  0,  0,  0, -5,
+                        -5,  0,  0,  0,  0,  0,  0, -5,
+                        -5,  0,  0,  0,  0,  0,  0, -5,
+                        0,  0,  0,  5,  5,  0,  0,  0
+                    ],
+                    chess.QUEEN: [
+                        -20,-10,-10, -5, -5,-10,-10,-20,
+                        -10,  0,  0,  0,  0,  0,  0,-10,
+                        -10,  0,  5,  5,  5,  5,  0,-10,
+                        -5,  0,  5,  5,  5,  5,  0, -5,
+                        0,  0,  5,  5,  5,  5,  0, -5,
+                        -10,  5,  5,  5,  5,  5,  0,-10,
+                        -10,  0,  5,  0,  0,  0,  0,-10,
+                        -20,-10,-10, -5, -5,-10,-10,-20
+                    ],
+                    chess.KING: [
+                        -30,-40,-40,-50,-50,-40,-40,-30,
+                        -30,-40,-40,-50,-50,-40,-40,-30,
+                        -30,-40,-40,-50,-50,-40,-40,-30,
+                        -30,-40,-40,-50,-50,-40,-40,-30,
+                        -20,-30,-30,-40,-40,-30,-30,-20,
+                        -10,-20,-20,-20,-20,-20,-20,-10,
+                        20, 20,  0,  0,  0,  0, 20, 20,
+                        20, 30, 10,  0,  0, 10, 30, 20
+                    ]
+                }
+                
+                score = 0
+                for piece_type in chess.PIECE_TYPES:
+                    white_squares = self.board.pieces(piece_type, chess.WHITE)
+                    black_squares = self.board.pieces(piece_type, chess.BLACK)
+
+                    material_value = piece_values[piece_type]
+
+                    for square in white_squares:
+                        # we mirror the square because python-chess puts A1 at 0, and would make our piece
+                        # square tables upside down if we didn't mirror it.
+                        mirrored_square = chess.square_mirror(square)
+                        score += material_value + PIECE_SQUARE_TABLES[piece_type][mirrored_square]
+
+                    for square in black_squares:
+                        score -= material_value + PIECE_SQUARE_TABLES[piece_type][square]
+
+                    
                 return score
