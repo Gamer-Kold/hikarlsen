10f4cea5dc6b48d70e6a561b96e798cae15d4026
Kold <myself@koldinium.com>
2025-12-13 05:12:51 +0500

    Basic Negamax

--- engine.py
+++ engine.py
@@ -8,3 +8,22 @@ class Engine:
         self.board = chess.Board(position)
         for move in moves:
             board.push_uci(move)
+
+    def negamax(self, depth):
+        if depth == 0 or self.board.is_game_over():
+            # negate evaluation if it's black's turn
+            # this allow's the evaluation function to be absolute
+            val = self.evaluate() * (1 if self.board.turn else -1)
+            return val
+        # negative infinity is always lower than 
+        # all integers so it's useful as an initial value
+        val = float("-inf") 
+
+        for candidate_move in self.board.legal_moves:
+            self.board.push(move) # make the move on the board
+            val = max(val, -self.negamax(depth - 1)) # evaluate new position
+            self.board.pop() # unmake move
+            return val # return
+
+    def evaluate(self):
+        pass
